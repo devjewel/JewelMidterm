f617563197d065ab9d7d55764702eb7bb387f636
devjewel <devjewel05@gmail.com>
2018-12-31 01:26:00 -0500

    Ans of two datastructure problem

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +13,8 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+
+
 
 	}
 
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,7 +12,22 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<String> queue = new LinkedList<>();
+		queue.add("Java");
+		queue.add("Python");
+		queue.add("Ruby");
+		String fifo = queue.peek();
 
+		String lilo = queue.remove();
+		System.out.println("Java should be remove : " + lilo);
+		for (String pl : queue){
+			System.out.println(pl);
+
+		}
+		Iterator it = queue.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
 	}
 
 }
